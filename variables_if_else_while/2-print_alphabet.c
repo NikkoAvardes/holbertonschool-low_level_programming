2326cabba723b0116f72768ed9d13ece465e048d
Nicolai Carabet <nicolaicarabet@MacBook-Air-Nicolai.local>
2025-06-17 19:44:17 +0200

    prtints the alphabet in lowercase

---
+++ variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+* main - Prints the alphabet in lowercase.
+*
+* Return: Always 0 (Success)
+*/
+int main(void)
+{
+for (char c = 'a'; c <= 'z'; c++)
+{
+putchar(c);
+}
+return (0);
+}
