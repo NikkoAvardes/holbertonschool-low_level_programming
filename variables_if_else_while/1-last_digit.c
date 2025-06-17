6e1150fccb33a9b80e39ebdc318135ab3020f2d6
Nicolai Carabet <nicolaicarabet@MacBookAir.hbtn.fr>
2025-06-17 16:38:22 +0200

    the last digit

--- variables_if_else_while/1-last_digit.c
+++ variables_if_else_while/1-last_digit.c
@@ -13,18 +13,17 @@ int last_digit;
 srand(time(0));
 n = rand() - RAND_MAX / 2;
 last_digit = abs(n % 10);
-printf("Last digit of %d ", last_digit);
 if (last_digit > 5)
 {
-printf("and is greater than 5");
+printf("Last digit of %d is %d and is greater than 5", n, last_digit);
 }
 else if (last_digit == 0)
 {
-printf("and is 0 ");
+printf("Last digit of %d is %d and is 0 ", n, last_digit);
 }
 if (last_digit < 6)
 {
-printf("and is less than 6 and not 0");
+printf("Last digit of %d is %d and is less than 6 and not 0", n, last_digit);
 }
 return (0);
 }
