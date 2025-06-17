f2ab88fc543f0e314299af00f230249d210127a5
Nicolai Carabet <nicolaicarabet@MacBookAir.hbtn.fr>
2025-06-17 11:26:26 +0200

    try to push

---
+++ variables_if_else_while/0-positive_or_negative.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+/**
+ * main - Entry point
+ * Return: Always 0 (Success)
+ */
+int main()
+{
+    int n;
+
+    n = 0; 
+    
+    if (n > 0)
+    {
+        printf("%d is positive\n", n);
+    }
+    else if (n < 0)
+    {
+        printf("%d is negative\n", n);
+    }
+    else
+    {
+        printf("%d is zero\n", n);
+    }
+
+    return (0);
+}
