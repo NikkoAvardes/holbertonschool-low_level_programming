0679a4db6f9c2a0db2b588d15fa6a56e5fdadfe1
Nicolai Carabet <nicolaicarabet@MacBookAir.hbtn.fr>
2025-06-20 10:00:13 +0200

    prints _putchar

--- functions_nested_loops/0-putchar.c
+++ functions_nested_loops/0-putchar.c
@@ -1,3 +1,4 @@
+#include <unistd.h>
 #include <stdio.h>
 /**
 * main - Entry point
@@ -6,6 +7,14 @@
 */
 int main(void)
 {
-printf("_putchar\n");
+putchar('_');
+putchar('p');
+putchar('u');
+putchar('t');
+putchar('c');
+putchar('h');
+putchar('a');
+putchar('r');
+putchar('\n');
 return (0);
 }
