0a3a6bbd530cc214cc4b79d0e9e8b53b8d35c336
Nicolai Carabet <nicolaicarabet@MacBookAir.hbtn.fr>
2025-06-20 16:13:39 +0200

    always positive numbers

---
+++ functions_nested_loops/7-print_last_digit.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdlib.h>
+/**
+ * print_last_digit - Computes the last digit of a number
+ * @n: The integer to evaluate
+ *
+ * Return: The last digit of the number
+ */
+int print_last_digit(int n)
+{
+int last_digit = n % 10;
+if (last_digit < 0)
+last_digit = -last_digit;
+return (last_digit);
+}
