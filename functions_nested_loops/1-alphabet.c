c04f61d3c29aaf40a57c7f2dc8d92874b012cde8
Nicolai Carabet <nicolaicarabet@MacBookAir.hbtn.fr>
2025-06-20 11:22:25 +0200

    I sometimes suffer from insomnia

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,15 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_alphabet - prints alphabets lowercase
+ */
+void print_alphabet(void)
+{
+char c;
+
+for (c = 'a'; c <= 'z'; c++)
+{
+_putchar(c);
+}
+_putchar('\n');
+}
