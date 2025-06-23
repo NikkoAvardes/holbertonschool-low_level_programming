ce13aba41a7a829bfab027f9b2fe3985607e1d9c
Nicolai Carabet <nicolaicarabet@macbookair.home>
2025-06-23 22:24:33 +0200

    Fizz-Buzz

--- more_functions_nested_loops/9-fizz_buzz.c
+++ more_functions_nested_loops/9-fizz_buzz.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 #include "main.h"
 /**
- * fizz_buzz - prints the numbers from 1 to 100, but for multiples of three
+ * main - prints the numbers from 1 to 100, but for multiples of three
  * prints "Fizz" instead of the number and for the multiples of five
  * prints "Buzz". For numbers which are multiples of both three and five,
  * prints "FizzBuzz".
@@ -10,25 +10,25 @@
  */
 int main(void)
 {
-    int n;
-    for (n = 1; n <= 100; n++)
-    {
-        if (n % 3 == 0 && n % 5 == 0)
-        {
-            printf("FizzBuzz ");
-        }
-        else if (n % 3 == 0)
-        {
-            printf("Fizz ");
-        }
-        else if (n % 5 == 0)
-        {
-            printf("Buzz ");
-        }
-        else
-        {
-            printf("%d ", n);
-        }
-    }
-    printf("\n");
+int n;
+for (n = 1; n <= 100; n++)
+{
+if (n % 3 == 0 && n % 5 == 0)
+{
+printf("FizzBuzz ");
+}
+else if (n % 3 == 0)
+{
+printf("Fizz ");
+}
+else if (n % 5 == 0)
+{
+printf("Buzz ");
+}
+else
+{
+printf("%d ", n);
+}
+}
+printf("\n");
 }
