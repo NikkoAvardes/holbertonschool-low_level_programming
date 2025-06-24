8c672850f0b8b34def1ae85d38da3c7dda78b6b7
Nicolai Carabet <nicolaicarabet@MacBookAir.hbtn.fr>
2025-06-24 10:05:57 +0200

    Triangles

--- more_functions_nested_loops/10-print_triangle.c
+++ more_functions_nested_loops/10-print_triangle.c
@@ -8,24 +8,24 @@
  */
 void print_triangle(int size)
 {
-    int i, j;
-    if (size <= 0)
-    {
-        _putchar ('\n');
-    }
-    else if (size > 0)
-    {
-        for (i = 1; i <= size; i++) // line
-        {
-            for (j = 0; j < size - i; j++) // espace
-            {
-                _putchar(' ');
-            }
-            for (j = 0; j < i; j++) // #
-            {
-                _putchar('#');
-            }
-            _putchar('\n');
-        }
-    }
+int i, j;
+if (size <= 0)
+{
+_putchar ('\n');
+}
+else if (size > 0)
+{
+for (i = 1; i <= size; i++)
+{
+for (j = 0; j < size - i; j++)
+{
+_putchar(' ');
+}
+for (j = 0; j < i; j++)
+{
+_putchar('#');
+}
+_putchar('\n');
+}
+}
 }
