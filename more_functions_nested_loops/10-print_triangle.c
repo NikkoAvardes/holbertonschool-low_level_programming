c8794f4ff3bc0d2c48518e6aa8b995f679bc9fb2
Nicolai Carabet <nicolaicarabet@MacBookAir.hbtn.fr>
2025-06-24 10:05:00 +0200

    Triangles

---
+++ more_functions_nested_loops/10-print_triangle.c
@@ -0,0 +1,31 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * print_triangle - prints a triangle of size 'size'
+ * @size: the size of the triangle
+ *
+ * Return: void
+ */
+void print_triangle(int size)
+{
+    int i, j;
+    if (size <= 0)
+    {
+        _putchar ('\n');
+    }
+    else if (size > 0)
+    {
+        for (i = 1; i <= size; i++) // line
+        {
+            for (j = 0; j < size - i; j++) // espace
+            {
+                _putchar(' ');
+            }
+            for (j = 0; j < i; j++) // #
+            {
+                _putchar('#');
+            }
+            _putchar('\n');
+        }
+    }
+}
