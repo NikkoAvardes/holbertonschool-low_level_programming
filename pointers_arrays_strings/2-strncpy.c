89b13caa4ef98100163c59c83ef9fc9719376285
Nicolai Carabet <nicolaicarabet@MacBookAir.hbtn.fr>
2025-06-27 14:38:07 +0200

    strncpy

--- pointers_arrays_strings/2-strncpy.c
+++ pointers_arrays_strings/2-strncpy.c
@@ -1,17 +1,25 @@
 #include "main.h"
 #include <stdio.h>
+/**
+ * _strncpy - copies up to n characters
+ * @dest: destination string
+ * @src: source string
+ * @n: number of characters to copy from src
+ *
+ * Return: pointer to the resulting string dest
+ */
 char *_strncpy(char *dest, char *src, int n)
 {
-    int i = 0;
-    while (i < n && src[i] != '\0')
-    {
-    dest[i] = src[i];
-    i++;
-    }
-    while (i < n)
-    {
-        dest[i] = '\0';
-        i++;
-    }
-    return (dest);
+int i = 0;
+while (i < n && src[i] != '\0')
+{
+dest[i] = src[i];
+i++;
+}
+while (i < n)
+{
+dest[i] = '\0';
+i++;
+}
+return (dest);
 }
