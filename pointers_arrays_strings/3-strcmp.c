4b199171eb505a91cd78d80b3907cafc9a422de7
Nicolai Carabet <nicolaicarabet@MacBookAir.hbtn.fr>
2025-06-27 15:11:24 +0200

    strcmp

--- pointers_arrays_strings/3-strcmp.c
+++ pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,13 @@
  */
 int _strcmp(char *s1, char *s2)
 {
-   int i = 0;
-    while (s1[i] != '\0' && s2[i] != '\0')
-    {
-    if (s1[i] != s2[i])
-        return (s1[i] - s2[i]);
-    i++;
-    }
+int i = 0;
+while (s1[i] != '\0' && s2[i] != '\0')
+{
+if (s1[i] != s2[i])
+return (s1[i] - s2[i]);
+i++;
+}
 
 return (s1[i] - s2[i]);
 }
