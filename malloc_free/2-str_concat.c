be6e43192f4b089b8e53930a408847d6b6eb056d
Nicolai Carabet <nicolaicarabet@MacBookAir.hbtn.fr>
2025-07-07 15:50:53 +0200

    malloc2

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -10,35 +10,34 @@
  */
 char *str_concat(char *s1, char *s2)
 {
-	char *ptr;
-	int i;
-	int j;
-	int len1 = 0;
-	int len2 = 0;
+char *ptr;
+int i;
+int j;
+int len1 = 0;
+int len2 = 0;
 
-	if (s1 == NULL)
-		s1 = "";
-	if (s2 == NULL)
-		s2 = "";
-	while (s1[len1] != '\0')
-	{
-		len1++;
-	}
-	while (s2[len2] != '\0')
-	{
-		len2++;
-	}
-	ptr = malloc(sizeof(char) * (len1 + len2 + 1));
-	if (ptr == NULL)
-		return (NULL);
+if (s1 == NULL)
+s1 = "";
+if (s2 == NULL)
+s2 = "";
 
-	for (i = 0; i < len1; i++)
-		ptr[i] = s1[i];
+while (s1[len1] != '\0')
+len1++;
 
-	for (j = 0; j < len2; j++)
-		{
-			ptr[len1 + j] = s2[j];
-		}
-			ptr[len1 + len2] = '\0';
-		return (ptr);
+while (s2[len2] != '\0')
+len2++;
+
+ptr = malloc(sizeof(char) * (len1 + len2 + 1));
+if (ptr == NULL)
+return (NULL);
+
+for (i = 0; i < len1; i++)
+ptr[i] = s1[i];
+
+for (j = 0; j < len2; j++)
+{
+ptr[len1 + j] = s2[j];
+}
+ptr[len1 + len2] = '\0';
+return (ptr);
 }
