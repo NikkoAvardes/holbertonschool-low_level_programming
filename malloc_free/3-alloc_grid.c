82bc0eb70cd98f2f0e9136ebb9e83f054a125505
Nicolai Carabet <nicolaicarabet@MacBookAir.hbtn.fr>
2025-07-08 14:11:09 +0200

    malloc3

--- malloc_free/3-alloc_grid.c
+++ malloc_free/3-alloc_grid.c
@@ -11,33 +11,29 @@
  */
 int **alloc_grid(int width, int height)
 {
-int **array;
-int i;
-int j;
+	int **array;
+	int i;
+	int j;
 
-if (width <= 0 || height <= 0)
-return (NULL);
+	if (width <= 0 || height <= 0)
+		return (NULL);
 
-array = malloc(height * sizeof(int *));
-if (array == NULL)
-return (NULL);
+	array = malloc(height * sizeof(int *));
+	if (array == NULL)
+		return (NULL);
 
-for (i = 0; i < height; i++)
-{
-array[i] = malloc(width * sizeof(int));
-if (array[i] == NULL)
-{
-while (i--)
-{
-free(array[i]);
-free(array);
-return (NULL);
-}
-}
-for (j = 0; j < width; j++)
-{
-array[i][j] = 0;
-}
-}
-return (array);
+	for (i = 0; i < height; i++)
+	{
+		array[i] = malloc(width * sizeof(int));
+		if (array[i] == NULL)
+		{
+			while (i--)
+				free(array[i]);
+			free(array);
+			return (NULL);
+		}
+		for (j = 0; j < width; j++)
+			array[i][j] = 0;
+	}
+	return (array);
 }
