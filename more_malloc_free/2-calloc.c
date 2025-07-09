1ba5f58f148937ac9cbf5010944fde11186226db
Nicolai Carabet <nicolaicarabet@MacBookAir.hbtn.fr>
2025-07-09 14:21:17 +0200

    calloc

---
+++ more_malloc_free/2-calloc.c
@@ -0,0 +1,26 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+/**
+ *  _calloc - allocates memory for an array, using malloc
+ * @nmemb: number of elements in the array
+ * @size: size of each element in the array
+ *
+ * Return: pointer to the allocated memory, or NULL if it fails
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	void *arr;
+
+	if (nmemb == 0)
+		return (NULL);
+	if (size == 0)
+		return (NULL);
+	arr = malloc(nmemb * size);
+
+	if (arr == NULL)
+		return (NULL);
+	memset(arr, 0, nmemb * size);
+		return (arr);
+}
