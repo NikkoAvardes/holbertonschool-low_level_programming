fdafc14495b37150fb64f355bacdab63689e2e10
Nicolai Carabet <nicolaicarabet@MacBookAir.hbtn.fr>
2025-07-10 10:00:42 +0200

    Poppy

---
+++ structures_typedef/dog.h
@@ -0,0 +1,21 @@
+#ifndef DOG_H
+#define DOG_H
+
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * struct dog - structure to define a dog
+ * @name: name of the dog
+ * @age: age of the dog
+ * @owner: owner of the dog
+ *
+ * Description: This structure defines a dog with its name, age, and owner.
+ */
+struct dog
+{
+char *name;
+float age;
+char *owner;
+};
+
+#endif
