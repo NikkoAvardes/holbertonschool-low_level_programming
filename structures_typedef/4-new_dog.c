766a6cd08fda4d65251045d12a1287108b2d859e
Nicolai Carabet <nicolaicarabet@macbookair.home>
2025-07-11 20:59:55 +0200

    Poppy4

--- structures_typedef/4-new_dog.c
+++ structures_typedef/4-new_dog.c
@@ -12,29 +12,31 @@
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *my_dog;
+	int i;
 	int name_len = 0;
 	int owner_len = 0;
 
 	while (name[name_len] != '\0')
-	{
 		name_len++;
-	}
+
 	while (owner[owner_len] != '\0')
-	{
 		owner_len++;
-	}
+
 	my_dog = malloc(sizeof(dog_t));
 	if (my_dog == NULL)
-	{
 		return (NULL);
-	}
+
 	my_dog->name = malloc(strlen(name) + 1);
 	if (my_dog->name == NULL)
 	{
 		free(my_dog);
 		return (NULL);
 	}
-	strcpy(my_dog->name, name);
+	for (i = 0; name[i] != '\0'; i++)
+	{
+		my_dog->name[i] = name[i];
+	}
+	my_dog->name[i] = '\0';
 	my_dog->owner = malloc(strlen(owner) + 1);
 	if (my_dog->owner == NULL)
 	{
@@ -42,7 +44,11 @@ dog_t *new_dog(char *name, float age, char *owner)
 		free(my_dog);
 		return (NULL);
 	}
-	strcpy(my_dog->owner, owner);
+	for (i = 0; owner[i] != '\0'; i++)
+	{
+		my_dog->owner[i] = owner[i];
+	}
+	my_dog->owner[i] = '\0';
 	my_dog->age = age;
 	return (my_dog);
 }
