ffb269e38f93250a08a4d61b94a8817867b7cae1
Nicolai Carabet <nicolaicarabet@macbookair.home>
2025-07-14 13:13:26 +0200

    To hell with circumstances; I create opportunities

---
+++ function_pointers/2-int_index.c
@@ -0,0 +1,26 @@
+#include "function_pointers.h"
+#include <stdio.h>
+int int_index(int *array, int size, int (*cmp)(int))
+{
+    int i = 0;
+
+    if (size <= 0)
+    {
+        return (-1);
+    }
+
+    if (array == NULL || cmp == NULL)
+    {
+        return (-1);
+    }
+
+    while (i < size)
+    {
+        if (cmp(array[i]))
+        {
+            return (i);
+        }
+        i++;
+    }
+    return (-1);
+}
