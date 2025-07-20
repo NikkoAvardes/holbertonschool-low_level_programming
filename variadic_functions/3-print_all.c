c8c36683df80027495e097cd9cf563bc2c1119be
Nicolai Carabet <nicolaicarabet@macbookair.home>
2025-07-20 17:56:32 +0200

    print diferents type

--- variadic_functions/3-print_all.c
+++ variadic_functions/3-print_all.c
@@ -4,48 +4,47 @@
 #include <stdarg.h>
 #include <string.h>
 /**
-* print_all - Prints anything based on the format specifier.
+ * print_all - Prints anything based on the format specifier.
  * @format: A string containing the format specifiers.
- *
- * Return: Nothing.
  */
 void print_all(const char * const format, ...)
-{
-	va_list args;
-	int i = 0;
-	int ap;
+{	va_list args;
+	int i = 0, ap;
 	double f;
-	char *s;
-	char *separator = "";
+	char *s, *separator = "";
 
 	va_start(args, format);
 	while (format[i] != '\0')
 	{
 	switch (format[i])
-	{
-	case 'c':
-		ap = va_arg(args, int);
-		printf("%c, ", ap);
-		break;
-	case 'i':
-		ap = va_arg(args, int);
-		printf("%i, ", ap);
-		break;
-	case 'f':
-		printf("%s", separator);
-		f = va_arg(args, double);
-		printf("%f", f);
-		separator = ", ";
-		break;
-	case 's':
-		printf("%s", separator);
-		s = va_arg(args, char *);
-		if (s == NULL)
-			s = "(nil)";
-		printf("%s", s);
-		separator = ", ";
-		break;
-	}
+		{
+		case 'c':
+			printf("%s", separator);
+			ap = va_arg(args, int);
+			printf("%c", ap);
+			separator = ", ";
+			break;
+		case 'i':
+			printf("%s", separator);
+			ap = va_arg(args, int);
+			printf("%i", ap);
+			separator = ", ";
+			break;
+		case 'f':
+			printf("%s", separator);
+			f = va_arg(args, double);
+			printf("%f", f);
+			separator = ", ";
+			break;
+		case 's':
+			printf("%s", separator);
+			s = va_arg(args, char *);
+			if (s == NULL)
+				s = "(nil)";
+			printf("%s", s);
+			separator = ", ";
+			break;
+		}
 		i++;
 	}
 	va_end(args);
