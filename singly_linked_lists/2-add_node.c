4fe1a5b6c152b5a4293429b92aa44f30aa191ae3
NikkoAvardes <11460@holbertonstudents.com>
2025-07-29 09:19:33 +0200

    ajout de la vérification de NULL pour le pointeur head dans la fonction add_node

--- singly_linked_lists/2-add_node.c
+++ singly_linked_lists/2-add_node.c
@@ -13,15 +13,15 @@ list_t *add_node(list_t **head, const char *str)
 {
 	list_t *new = malloc(sizeof(list_t));
 
+	new->str = strdup(str);
+	new->len = strlen(str);
+	new->next = *head;
+
 	if (head == NULL || new == NULL)
 	{
 		free(new);
 		return (NULL);
 	}
-	new->str = strdup(str);
-	new->len = strlen(str);
-	new->next = *head;
-
 	*head = new;
 	return (new);
 }
