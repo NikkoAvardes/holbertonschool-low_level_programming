cd3107755db92a0d58cc0be882a5813fcab94e9c
NikkoAvardes <11460@holbertonstudents.com>
2025-07-30 11:33:59 +0200

    suppression des appels à free pour éviter la libération prématurée de la mémoire dans la fonction delete_dnodeint_at_index

--- doubly_linked_lists/8-delete_dnodeint.c
+++ doubly_linked_lists/8-delete_dnodeint.c
@@ -26,7 +26,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		{
 			(*head)->prev = NULL;
 		}
-		free(tmp);
 		return (1);
 	}
 
@@ -42,7 +41,6 @@ int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 		{
 			tmp->next->prev = prev;
 		}
-		free(tmp);
 		return (1);
 	}
 	else
