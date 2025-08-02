bc129504362ec01d09cba6c57c68794f68729ea7
NikkoAvardes <11460@holbertonstudents.com>
2025-08-02 13:15:40 +0200

    fix: ajout d'une vérification pour le malloc dans la création de la table de hachage

--- hash_tables/0-hash_table_create.c
+++ hash_tables/0-hash_table_create.c
@@ -13,7 +13,8 @@ hash_table_t *hash_table_create(unsigned long int size)
 	hash_table_t *table;
 
 	table = malloc(sizeof(hash_table_t));
-
+	if (table == NULL)
+		return (NULL);
 	table->size = size;
 	table->array = calloc(size, sizeof(hash_node_t *));
 	if (table->array == NULL)
