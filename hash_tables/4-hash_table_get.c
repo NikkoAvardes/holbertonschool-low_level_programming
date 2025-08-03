cf7a92a25a53bcbafd1ba0863811a5d624fd2836
NikkoAvardes <11460@holbertonstudents.com>
2025-08-03 19:29:42 +0200

    ajout: implémentation des fonctions hash_table_print et hash_table_delete, et mise à jour des fichiers main pour les tests

--- hash_tables/4-hash_table_get.c
+++ hash_tables/4-hash_table_get.c
@@ -2,6 +2,13 @@
 #include <string.h>
 #include <stdio.h>
 #include "hash_tables.h"
+/**
+ * hash_table_get - Retrieves a value associated with a key in a hash table.
+ * @ht: The hash table to look into.
+ * @key: The key to search for.
+ *
+ * Return: The value associated with the key, or NULL if the key is not found.
+ */
 char *hash_table_get(const hash_table_t *ht, const char *key)
 {
 	unsigned long int index;
